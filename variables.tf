a58ee9384b5a2c1f4817752e66b03ec6d9c99ce5
Jeremiah Brown <jeremiahbrown1288@gmail.com>
2026-08-11 15:28:24 -0400

    Add variables for monitoring configuration

--- variables.tf
+++ variables.tf
@@ -1,1 +1,32 @@
+variable "yourname" {
+  type = string
+}
+ 
+variable "location" {
+  type    = string
+  default = "East US"
+}
+ 
+variable "target_url" {
+  description = "The website URL to monitor. Include https://."
+  type        = string
+}
+ 
+variable "alert_email" {
+  description = "Email address for downtime alerts."
+  type        = string
+}
+ 
+variable "alert_phone" {
+  description = "Phone number for SMS alerts in E.164 format (e.g. +14045550100)."
+  type        = string
+}
+ 
+variable "tags" {
+  type = map(string)
+  default = {
+    project    = "uptime-monitor"
+    managed_by = "terraform"
+  }
+}
 
